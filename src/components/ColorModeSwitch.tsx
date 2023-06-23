de616662f04c659e751631eb16015441491b8b61
Jack McCauley <104650642+JJMcCauley@users.noreply.github.com>
2023-06-23 14:17:22 -0500

    Build the color mode switch and implemented it to navbar

---
+++ src/components/ColorModeSwitch.tsx
@@ -0,0 +1,19 @@
+import { Switch, HStack, useColorMode, Text } from "@chakra-ui/react";
+
+const ColorModeSwitch = () => {
+  const { toggleColorMode, colorMode } = useColorMode();
+  const msg = `Switch to ${colorMode === "dark" ? "Light Mode" : "Dark Mode"}`;
+
+  return (
+    <HStack>
+      <Switch
+        colorScheme="green"
+        isChecked={colorMode === "dark"}
+        onChange={toggleColorMode}
+      />
+      <Text>{msg}</Text>
+    </HStack>
+  );
+};
+
+export default ColorModeSwitch;
